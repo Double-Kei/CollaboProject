b1c8a5fbe42edcafb1ea277702f67d3c33ea6c27
amanaksu <amanaksu@gmail.com>
2022-09-30 20:49:54 +0900

    add scroll property in tabs

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,24 +12,41 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: 9,
       child: Scaffold(
         appBar: AppBar(
           title: const CupertinoSearchTextField(),
           elevation: 0,
           backgroundColor: Colors.white,
           bottom: const TabBar(
+            isScrollable: true,
+            indicatorColor: Colors.yellow,
+            indicatorWeight: 8,
             tabs: [
+              RepeatedTab(label: "Accessories",),
+              RepeatedTab(label: "Bags",),
+              RepeatedTab(label: "Beauty",),
+              RepeatedTab(label: "Electronics",),
+              RepeatedTab(label: "Home & Garden",),
+              RepeatedTab(label: "Inapp",),
+              RepeatedTab(label: "Kids",),
               RepeatedTab(label: "Men",),
-              RepeatedTab(label: "Women",),
               RepeatedTab(label: "Shoes",),
+              RepeatedTab(label: "Women",),
             ],
           ),
         ),
         body: const TabBarView(children: [
+          Center(child: Text("Accessories Page in Screen"),),
+          Center(child: Text("Bags Page in Screen"),),
+          Center(child: Text("Beauty Page in Screen"),),
+          Center(child: Text("Electronics Page in Screen"),),
+          Center(child: Text("Home & Garden Page in Screen"),),
+          Center(child: Text("Inapp Page in Screen"),),
+          Center(child: Text("Kids Page in Screen"),),
           Center(child: Text("Men Page in Screen"),),
-          Center(child: Text("Women Page in Screen"),),
           Center(child: Text("Shoes Page in Screen"),),
+          Center(child: Text("Women Page in Screen"),),
         ]),
       ),
     );
