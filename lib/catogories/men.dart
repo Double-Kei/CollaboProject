68853f89b86ef197af95afaf31765fc38024251a
amanaksu <amanaksu@gmail.com>
2022-10-11 16:23:19 +0900

    [ISSUE] 'Bottom Overflowed By 3.3 Pixels' by Product Resizing

--- lib/catogories/men.dart
+++ lib/catogories/men.dart
@@ -8,41 +8,64 @@ class MenCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(30.0),
-            child: Text("Men", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 1.5),),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.68,
-            child: GridView.count(
-              mainAxisSpacing: 70,
-              crossAxisSpacing: 15,
-              crossAxisCount: 3, 
-              children: List.generate(men.length, (index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => SubCategoryProducts(mainCategoryName: "Men",  subCategoryName: men[index],)));
-                  },
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 70, 
-                        width: 70,
-                        child: Image(image: AssetImage("images/men/men$index.jpg")),
-                      ),
-                      Text(men[index])
-                    ],
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 0,
+          left: 0,
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.8,
+            width: MediaQuery.of(context).size.width * 0.75,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: Text("Men", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 1.5),),
                   ),
-                );
-              }
-            ),),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.68,
+                    child: GridView.count(
+                      mainAxisSpacing: 70,
+                      crossAxisSpacing: 15,
+                      crossAxisCount: 3, 
+                      children: List.generate(men.length, (index) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => SubCategoryProducts(mainCategoryName: "Men",  subCategoryName: men[index],)));
+                          },
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 65, 
+                                width: 65,
+                                child: Image(image: AssetImage("images/men/men$index.jpg")),
+                              ),
+                              Text(men[index])
+                            ],
+                          ),
+                        );
+                      }
+                    ),),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ],
-      ),
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.8,
+            width: MediaQuery.of(context).size.width * 0.05,
+            child: Container(
+              decoration: BoxDecoration(color: Colors.brown.withOpacity(0.2)),
+            ),
+          )
+        )
+      ],
     );
   }
 }
